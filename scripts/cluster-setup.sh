853e22d209cc0dbbe88afd27c0e7743747128b33
moueslati <eng.oueslati.mohamed@gmail.com>
2026-08-04 09:33:07 +0200

    Create a script to add prometheus to kube cluster with a separate namespace

---
+++ scripts/cluster-setup.sh
@@ -0,0 +1,10 @@
+# Add the Helm repo
+# This will install It installs Prometheus, Grafana, node-exporter (hardware metrics), 
+# kube-state-metrics (pod health), and AlertManager in one command.
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+# Install into its own namespace
+helm install monitoring prometheus-community/kube-prometheus-stack \
+  --namespace monitoring \
+  --create-namespace
